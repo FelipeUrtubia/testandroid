6447512e2cf4422c5da91200870a8148b0502de1
Felipe Urtubia <pipe-3-97@hotmail.com>
2018-05-27 00:37:32 -0400

    Quinto Commit, agregado botón en el layaut 2 para seguir al layout 3

--- app/src/main/java/com/example/fxexlxixpe/testandroid/Main2Activity.java
+++ app/src/main/java/com/example/fxexlxixpe/testandroid/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.example.fxexlxixpe.testandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void cambiarVista (View view) {
+        Intent intetn = new Intent(this, MainActivity.class);
+        startActivity(intetn);
     }
 }
